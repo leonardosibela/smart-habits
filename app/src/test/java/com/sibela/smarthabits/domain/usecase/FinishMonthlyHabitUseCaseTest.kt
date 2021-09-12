b0033071330cf62635061751ab922bbd8efbd2c1
leonardosibela <leonardo_sibela@hotmail.com>
2021-09-12 18:24:23 -0300

    Implementing FinishMonthlyHabitUseCaseTest

---
+++ app/src/test/java/com/sibela/smarthabits/domain/usecase/FinishMonthlyHabitUseCaseTest.kt
@@ -0,0 +1,32 @@
+package com.sibela.smarthabits.domain.usecase
+
+import com.sibela.smarthabits.domain.repository.MonthlyHabitRepository
+import com.sibela.smarthabits.util.TestData.FIRST_MONTHLY_HABIT
+import com.sibela.smarthabits.util.initMockKAnnotations
+import io.mockk.coJustRun
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class FinishMonthlyHabitUseCaseTest {
+
+    @RelaxedMockK
+    private lateinit var monthlyHabitRepository: MonthlyHabitRepository
+
+    @InjectMockKs
+    private lateinit var finishMonthlyHabitUseCase: FinishMonthlyHabitUseCase
+
+    init {
+        initMockKAnnotations()
+    }
+
+    @Test
+    fun invoke() = runBlocking {
+        val monthlyHabit = FIRST_MONTHLY_HABIT
+        coJustRun { monthlyHabitRepository.remove(monthlyHabit) }
+        finishMonthlyHabitUseCase(monthlyHabit)
+        coVerify(exactly = 1) { monthlyHabitRepository.remove(monthlyHabit) }
+    }
+}
