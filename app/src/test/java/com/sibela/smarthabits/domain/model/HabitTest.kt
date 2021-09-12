ddae554337f39557512a60306d1621f740639295
leonardosibela <leonardo_sibela@hotmail.com>
2021-09-12 00:54:18 -0300

    Implementing HabitTest

---
+++ app/src/test/java/com/sibela/smarthabits/domain/model/HabitTest.kt
@@ -0,0 +1,18 @@
+package com.sibela.smarthabits.domain.model
+
+import org.junit.Assert
+import org.junit.Test
+
+class HabitTest {
+
+    @Test
+    fun instantiating() {
+        val id = 1
+        val description = "description"
+        val periodicity = Periodicity.DAILY
+        val dailyHabit = Habit(id, description, periodicity)
+        Assert.assertEquals(id, dailyHabit.id)
+        Assert.assertEquals(description, dailyHabit.description)
+        Assert.assertEquals(periodicity, dailyHabit.periodicity)
+    }
+}
