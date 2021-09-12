26df2601bd47f78d972d1f5b050103f948d11701
leonardosibela <leonardo_sibela@hotmail.com>
2021-09-12 11:41:46 -0300

    Implementing EditHabitViewModelTest

---
+++ app/src/test/java/com/sibela/smarthabits/presentation/viewmodel/EditHabitViewModelTest.kt
@@ -0,0 +1,40 @@
+package com.sibela.smarthabits.presentation.viewmodel
+
+import com.sibela.smarthabits.domain.usecase.EditHabitUseCase
+import com.sibela.smarthabits.util.CoroutineTestRule
+import com.sibela.smarthabits.util.TestData.FIRST_HABIT
+import com.sibela.smarthabits.util.initMockKAnnotations
+import io.mockk.coJustRun
+import io.mockk.coVerify
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class EditHabitViewModelTest {
+
+    @get:Rule
+    @ExperimentalCoroutinesApi
+    val coroutineTestRule = CoroutineTestRule()
+
+    @RelaxedMockK
+    private lateinit var editHabitUseCase: EditHabitUseCase
+
+    @InjectMockKs
+    private lateinit var editHabitViewModel: EditHabitViewModel
+
+    @Before
+    fun setUp() {
+        initMockKAnnotations()
+    }
+
+    @Test
+    fun editHabit() {
+        val habit = FIRST_HABIT
+        coJustRun { editHabitUseCase(habit) }
+        editHabitViewModel.editHabit(habit)
+        coVerify(exactly = 1) { editHabitUseCase(habit) }
+    }
+}
