e4e9a3c8eb581e5f250d54a9e352f2f046c6ef95
leonardosibela <leonardo_sibela@hotmail.com>
2021-09-12 00:58:11 -0300

    Implementing MonthlyHabitTest

---
+++ app/src/test/java/com/sibela/smarthabits/domain/model/MonthlyHabitTest.kt
@@ -0,0 +1,20 @@
+package com.sibela.smarthabits.domain.model
+
+import org.junit.Assert
+import org.junit.Test
+
+class MonthlyHabitTest {
+
+    @Test
+    fun instantiating() {
+        val id = 1
+        val description = "description"
+        val completed = false
+        val period = 2
+        val monthlyHabit = MonthlyHabit(id, description, completed, period)
+        Assert.assertEquals(id, monthlyHabit.id)
+        Assert.assertEquals(description, monthlyHabit.description)
+        Assert.assertEquals(completed, monthlyHabit.completed)
+        Assert.assertEquals(period, monthlyHabit.period)
+    }
+}
