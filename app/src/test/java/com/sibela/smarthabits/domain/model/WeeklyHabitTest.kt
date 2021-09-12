4dede4c1ce3669dd9fac203a6fa05e20b6e04a03
leonardosibela <leonardo_sibela@hotmail.com>
2021-09-12 00:57:14 -0300

    Implementing WeeklyHabitTest

---
+++ app/src/test/java/com/sibela/smarthabits/domain/model/WeeklyHabitTest.kt
@@ -0,0 +1,20 @@
+package com.sibela.smarthabits.domain.model
+
+import org.junit.Assert
+import org.junit.Test
+
+class WeeklyHabitTest {
+
+    @Test
+    fun instantiating() {
+        val id = 1
+        val description = "description"
+        val completed = false
+        val period = 2
+        val weeklyHabit = WeeklyHabit(id, description, completed, period)
+        Assert.assertEquals(id, weeklyHabit.id)
+        Assert.assertEquals(description, weeklyHabit.description)
+        Assert.assertEquals(completed, weeklyHabit.completed)
+        Assert.assertEquals(period, weeklyHabit.period)
+    }
+}
